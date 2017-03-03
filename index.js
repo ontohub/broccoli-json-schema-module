dbff1cfd9b7034c0a4c96d53c7a992959b87fae5
Tom Gehrke <phyrog@cognitive-coding.com>
2017-03-03 23:00:04 +0100

    Add JSON schema dereferencing

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var Filter = require('broccoli-filter')
+var path = require('path');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var Filter = require('broccoli-filter');
+var deref = require('json-schema-deref');
 
 module.exports = JsonModule
 JsonModule.prototype = Object.create(Filter.prototype)
@@ -12,6 +16,39 @@ function JsonModule (inputTree, options) {
 JsonModule.prototype.extensions = ['json']
 JsonModule.prototype.targetExtension = 'js'
   
-JsonModule.prototype.processString = function (string) {
-  return "export default " + string + ";";
+JsonModule.prototype.processFile =
+    function processFile(srcDir, destDir, relativePath) {
+  var self = this;
+  var inputEncoding = this.inputEncoding;
+  var outputEncoding = this.outputEncoding;
+  if (inputEncoding === void 0) inputEncoding = 'utf8';
+  if (outputEncoding === void 0) outputEncoding = 'utf8';
+  var contents = fs.readFileSync(
+      srcDir + '/' + relativePath, { encoding: inputEncoding });
+
+  return Promise.resolve(this.processString(contents, relativePath, srcDir)).
+      then(function asyncOutputFilteredFile(outputString) {
+        var outputPath = self.getDestFilePath(relativePath);
+        if (outputPath == null) {
+          throw new Error('canProcessFile("' + relativePath + '") is true, but getDestFilePath("' + relativePath + '") is null');
+        }
+        outputPath = destDir + '/' + outputPath;
+        mkdirp.sync(path.dirname(outputPath));
+        fs.writeFileSync(outputPath, outputString, {
+          encoding: outputEncoding
+        });
+      });
+};
+
+JsonModule.prototype.processString = function (string, relativePath, srcDir) {
+  return new Promise(function(res, rej) {
+    var schema = JSON.parse(string);
+    deref(schema, { baseFolder: path.resolve(srcDir, path.dirname(relativePath)) }, (err, fullSchema) => {
+      if(err) {
+        rej(err);
+      } else {
+        res('export default ' + JSON.stringify(fullSchema) + ';');
+      }
+    });
+  });
 }
