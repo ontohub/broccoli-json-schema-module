1dd9c22b558b779ee528fabdf6214ae77bce7bdb
Tom Gehrke <phyrog@cognitive-coding.com>
2017-03-06 10:01:05 +0100

    Pass srcDir to processString

--- index.js
+++ index.js
@@ -13,10 +13,35 @@ function JsonModule (inputTree, options) {
 JsonModule.prototype.extensions = ['json']
 JsonModule.prototype.targetExtension = 'js'
   
-JsonModule.prototype.processString = function (string) {
+JsonModule.prototype.processFile =
+    function processFile(srcDir, destDir, relativePath) {
+  var self = this;
+  var inputEncoding = this.inputEncoding;
+  var outputEncoding = this.outputEncoding;
+  if (inputEncoding === void 0) inputEncoding = 'utf8';
+  if (outputEncoding === void 0) outputEncoding = 'utf8';
+  var contents = fs.readFileSync(
+      srcDir + '/' + relativePath, { encoding: inputEncoding });
+
+  return Promise.resolve(this.processString(contents, relativePath, srcDir)).
+      then(function asyncOutputFilteredFile(outputString) {
+        var outputPath = self.getDestFilePath(relativePath);
+        if (outputPath == null) {
+          throw new Error('canProcessFile("' + relativePath + '") is true, but getDestFilePath("' + relativePath + '") is null');
+        }
+        outputPath = destDir + '/' + outputPath;
+        mkdirp.sync(path.dirname(outputPath));
+        fs.writeFileSync(outputPath, outputString, {
+          encoding: outputEncoding
+        });
+      });
+};
+
+JsonModule.prototype.processString = function (string, relativePath, srcDir) {
+  console.log('Processing');
   return new Promise(function(res, rej) {
     var schema = JSON.parse(string);
-    deref(schema, 'app/schemas', (err, fullSchema) => {
+    deref(schema, srcDir, (err, fullSchema) => {
       if(err) {
         rej(err);
       } else {
